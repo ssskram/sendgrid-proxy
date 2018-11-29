47941869eb76a68d4bed40b19e9fc4b80ecf486b
ssskram <paul.marks@pittsburghpa.gov>
2018-11-29 11:40:41 -0500

    forwarding body from body of incoming post call

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -5,21 +5,13 @@ const router = express.Router()
 const checkToken = require('../token')
 const sgMail = require('@sendgrid/mail')
 
-const body = {
-  to: 'paul.marks@pittsburghpa.gov',
-  from: 'paul.marks@pittsburghpa.gov',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
-}
-
 // send single email
 router.post('/single',
   async function (req, res) {
     const valid = (checkToken(req.token))
     if (valid == true) {
       sgMail.setApiKey(process.env.SENDGRID)
-      const msg = body
+      const msg = req.body
       sgMail.send(msg)
     } else res.status(403).end()
   }
